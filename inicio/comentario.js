b485080ffc711217a674a9e9dd16f419a4da6011
MarcosDarpa <130925272+MarcosDarpa@users.noreply.github.com>
2023-04-27 22:30:09 -0300

    exercicios.js

---
+++ inicio/comentario.js
@@ -0,0 +1,10 @@
+//Esse é um comentário de uma linha
+//console.log é usado para exibir algo na tela do computador!
+
+console.log('Passo #01');
+console.log('Passo #02');
+console.log('Passo #03');  //Mais um exemplo de console.log!
+
+/*
+Esse é um comentário de várias linhas no JS
+*/
---
+++ inicio/sentenca_2.js
@@ -0,0 +1,3 @@
+console.log('Passo #01');
+console.log('Paaso #02');
+console.log('Passo #03');
